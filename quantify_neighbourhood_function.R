a1ebf5671a4911612d8e7f70f95e4d833cb0b0ab
Lei Qin <108281841+leiLQ@users.noreply.github.com>
2023-10-19 17:06:00 +1100

    Add files via upload

---
+++ quantify_neighbourhood_function.R
@@ -0,0 +1,43 @@
+library(Seurat)
+library(dplyr)
+library(tidyverse)
+
+# calculate the variance in coordinates of the neighbour cells for aech cell in a certain dimensionality reduction
+
+#'@title neighbour_distance.scaled.
+#'@description The function to calculate the average variance in coordinates of neighbour cells of a certain cell i based on the provided reduction map.
+#'@param i The cell index.
+#'@param reduction The reduction map used to calculate the coordinate variance.
+#'@param seu The seurat object.
+#'@return A number, the average variance in coordinates of neighbour cells of a certain cell i based on the provided reduction map.
+neighbour_distance.scaled = function(i, reduction, seu) {
+  # extract neighbour cells of cell i
+  n = colnames(seu@graphs$RNA_nn)[seu@graphs$RNA_nn[i,] == 1] # please change "RNA_nn" to "SCT_nn" if you are using SCTranform
+
+  # normalize dimension scales
+  embed.scale <- Embeddings(seu, reduction = reduction)
+  embed.scale[,1] <- BBmisc::normalize(embed.scale[,1], method = "range", range = c(-8, 8))
+  embed.scale[,2] <- BBmisc::normalize(embed.scale[,2], method = "range", range = c(-8, 8))
+
+  # dims of 20 cells
+  e = embed.scale[n, ]
+
+  # variance in dim
+  mean(var(e[,1]), var(e[,2]))
+}
+
+
+
+#'@title calculate_neighbour_distance_for_all_cells.
+#'@description The function to calculate the average variance in coordinates of neighbour cells of all cells in the seurat object based on the provided reduction map.
+#'@param colname The colname to store the neighbourhood distance value in metadata.
+#'@param reduction The reduction map used to calculate the coordinate variance.
+#'@param seu The seurat object.
+#'@return A seurat object with a new metadata column storing the varaince in coodinates of neighbour cells for each cell
+calculate_neighbour_distance_for_all_cells <- function(seu, reduction, colname) {
+  for(i in 1:nrow(seu@meta.data)) {seu@meta.data[[colname]][i] = neighbour_distance.scaled(i, reduction, seu)}
+}
+
+
+
+
---
+++ visualize_neighbourhood_function.R
@@ -0,0 +1,44 @@
+library(Seurat)
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+# plot neighbour cells of an individual cell or a certain cell group (i.e., cluster)
+
+#'@title visualize_neighbourhood.
+#'@description The function to highlight the neighbour cells of certain cells.
+#'@param meta_data_column Name of the column in seurat_object@meta.data slot to pull value from for highlighting.
+#'@param meta_data_highlight Name of variable(s) within meta_data_name to highlight in the plot.
+#'@param reduction The reduction map used to calculate the coordinate variance.
+#'@param seu The seurat object.
+#'@param density Whether to plot the density countour plot of neighbour cells. Default is FALSE.
+#'@return A number, the average variance in coordinates of neighbour cells of a certain cell i based on the provided reduction map.
+visualize_neighbourhood = function(seu, meta_data_column, meta_data_highlight, reduction, density = F) {
+  # all neighbour cells 
+  n = colnames(seu@graphs$RNA_nn)[colSums(seu@graphs$RNA_nn[seu[[meta_data_column]] == meta_data_highlight,]) > 0] # please change "RNA_nn" to "SCT_nn" if you are using SCTranform
+  
+  # reduction emeddings of these neighbour cells
+  Embeddings(seu, reduction = reduction) %>%
+    as_tibble(rownames = "bc") %>%
+    mutate(neighbour = bc %in% n) -> d
+  
+  # matching axis names to the reduction names, please adjust it according to the real case
+  axis <- gsub("umap","UMAP",gsub("dm", "DC", reduction))           
+  axis_1 <- rlang::sym(paste0(axis, "_1")) 
+  axis_2 <- rlang::sym(paste0(axis, "_2")) 
+  
+  if (!density){
+    # plot by just highlighting these neighbour cells in the reduction map
+    ggplot(d, aes(!!axis_1, !!axis_2)) +
+      geom_point(colour = "grey") +
+      geom_point(data = d[d$neighbour,], colour = "red", size = 0.6) +
+      theme_classic()+ ggtitle(paste0(meta_data_column, meta_data_highlight))
+    
+  } else{
+    # plot the density plot of these neighbour cells in the reduction map
+    ggplot(d, aes(!!axis_1, !!axis_2)) +
+      geom_point(colour = "grey") +
+      geom_density_2d(data = d[d$neighbour,]) +
+      theme_classic() + ggtitle(paste0(meta_data_column, meta_data_highlight))
+  }
+}
